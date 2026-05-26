26e3908c2dd855a145758bc76c1a13c2e86cfd8a
danorenciomar-maker <danorenciomar@gmail.com>
2026-05-26 14:28:28 +0800

    new

--- application/controllers/Pdf_converter.php
+++ application/controllers/Pdf_converter.php
@@ -102,7 +102,7 @@ class Dormitory_pdf extends CI_Controller
             /*
              * OCR the cropped SAN column.
              */
-            $ocr_text = $this->ocr_numbers_only($san_image);
+            $ocr_text = $this->ocr_numbers_only($san_image);;
 
             /*
              * Extract 4-digit SAN numbers.
