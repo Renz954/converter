e50e769f22d59adff503be2dfb1b53a936a0d48e
danorenciomar-maker <danorenciomar@gmail.com>
2026-05-28 10:56:23 +0800

    bago ni

--- application/views/converter_view.php
+++ application/views/converter_view.php
@@ -19,7 +19,7 @@
         <div class="panel-body">
 
             <p>
-                Click the button below to scan the uploaded PDF and extract all numberssss
+                Click the button below to scan the uploaded PDF and extract all numberssssaaaaasssssss
                 under the first <strong>SAN</strong> column into an Excel file.
             </p>
 
