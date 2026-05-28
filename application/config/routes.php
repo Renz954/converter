5f84af9fb2f1b02a261d2e0cef70ed636e605942
danorenciomar-maker <danorenciomar@gmail.com>
2026-05-28 14:41:32 +0800

    ssss

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,8 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['converter']   = 'pdf_converter/index';
-$route['convertpdf_converter/convert_to_excel']     = 'pdf_converter/convert_to_excel';
-$route['default_controller'] = 'pdf_converter/index';
+$route['default_controller'] = 'pdf_converter';
+
+$route['converter'] = 'pdf_converter/index';
+
+$route['convert_to_excel'] = 'pdf_converter/convert_to_excel';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
