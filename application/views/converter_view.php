15f6156a2e5c2e6ca21ee80addf7196779aac453
danorenciomar-maker <danorenciomar@gmail.com>
2026-05-28 11:17:43 +0800

    sss

--- application/views/converter_view.php
+++ application/views/converter_view.php
@@ -19,7 +19,7 @@
         <div class="panel-body">
 
             <p>
-                Click the button below to scan the uploaded PDF and extract all numbersss
+                Click the button below to scan the uploaded PDF and extract all number
                 under the first <strong>SAN</strong> column into an Excel file.
             </p>
 
